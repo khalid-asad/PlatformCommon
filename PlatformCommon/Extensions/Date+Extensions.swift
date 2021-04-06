05c847d5f937a3da871d5672e952cfd1b0c79d78
Khalid Asad <khalidasad93@gmail.com>
2021-04-06 00:23:51 -0400

    Added Date Extensions.

--- PlatformCommon/Extensions/Date+Extensions.swift
+++ PlatformCommon/Extensions/Date+Extensions.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Khalid Asad. All rights reserved.
 //
 
+// MARK: - Date Formats
 public extension Date {
     
     /// Converts a Date of format yyyy-MM-dd into a String of year.
@@ -20,5 +21,25 @@ public extension Date {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd/MM/yyyy"
         return dateFormatter.string(from: self)
+    }
+    
+    /// Converts a Date of format yyyy-MM-dd into a String of month year format (April 2012).
+    var toMonthYear: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
+
+// MARK: - Date Calculations
+public extension Date {
+    
+    /// Adds to a date given a value and type (day or month or etc).
+    func addToDate(value: Int, type: Calendar.Component) -> Date {
+        var dateComponent = DateComponents()
+        dateComponent.month = type == .month ? value : 0
+        dateComponent.day = type == .day ? value : 0
+        dateComponent.year = type == .year ? value : 0
+        return Calendar.current.date(byAdding: dateComponent, to: self) ?? self
     }
 }
